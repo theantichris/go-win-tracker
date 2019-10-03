a0cc612e766b4ba71bcc6039bd03213a8d367b8b
Christopher Lamm <chris@theantichris.com>
2019-10-03 17:23:50 -0400

    fleshed out InMemoryPlayerStore with integration test

--- main.go
+++ main.go
@@ -5,16 +5,25 @@ import (
 	"net/http"
 )
 
-type InMemoryPlayerStore struct{}
-
-func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
-	return 123
+type InMemoryPlayerStore struct {
+	store map[string]int
 }
 
-func (i *InMemoryPlayerStore) RecordWin(name string) {}
+func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
+	return i.store[name]
+}
+
+func (i *InMemoryPlayerStore) RecordWin(name string) {
+	i.store[name]++
+}
+
+func NewInMemoryPlayerStore() *InMemoryPlayerStore {
+	return &InMemoryPlayerStore{map[string]int{}}
+}
 
 func main() {
-	server := &PlayerServer{&InMemoryPlayerStore{}}
+	store := NewInMemoryPlayerStore()
+	server := &PlayerServer{store}
 
 	if err := http.ListenAndServe(":5000", server); err != nil {
 		log.Fatalf("could not listen on port 5000 %v", err)
---
+++ server_integration_test.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"net/http/httptest"
+	"testing"
+)
+
+func TestRecordingWinsAndRetrievingThem(t *testing.T) {
+	store := NewInMemoryPlayerStore()
+	server := PlayerServer{store}
+
+	player := "Pepper"
+
+	server.ServeHTTP(httptest.NewRecorder(), newPostWinRequest(player))
+	server.ServeHTTP(httptest.NewRecorder(), newPostWinRequest(player))
+	server.ServeHTTP(httptest.NewRecorder(), newPostWinRequest(player))
+
+	response := httptest.NewRecorder()
+	server.ServeHTTP(response, newGetScoreRequest("Pepper"))
+
+	assertResponseBody(t, response.Body.String(), "3")
+}
