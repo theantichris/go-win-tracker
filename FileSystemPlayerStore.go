2354471f2aaff6176bc5e44edaed26f264da78dc
Christopher Lamm <chris@theantichris.com>
2019-10-04 14:56:34 -0400

    GetPlayerScore

--- FileSystemPlayerStore.go
+++ FileSystemPlayerStore.go
@@ -14,3 +14,16 @@ func (f *FileSystemPlayerStore) GetLeague() []Player {
 
 	return league
 }
+
+func (f *FileSystemPlayerStore) GetPlayerScore(name string) int {
+	var wins int
+
+	for _, player := range f.GetLeague() {
+		if player.Name == name {
+			wins = player.Wins
+			break
+		}
+	}
+
+	return wins
+}
--- FileSystemPlayerStore_test.go
+++ FileSystemPlayerStore_test.go
@@ -26,4 +26,20 @@ func TestFileSystemStore(t *testing.T) {
 
 		assertLeague(t, got, want)
 	})
+
+	t.Run("get player score", func(t *testing.T) {
+		database := strings.NewReader(`[
+			{"Name": "Cleo", "Wins": 10},
+			{"Name": "Christopher", "Wins": 33}
+		]`)
+
+		store := FileSystemPlayerStore{database}
+
+		got := store.GetPlayerScore("Christopher")
+		want := 33
+
+		if got != want {
+			t.Errorf("got %d want %d", got, want)
+		}
+	})
 }
