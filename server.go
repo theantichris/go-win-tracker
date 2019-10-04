20c5c0b3a245af491161917a85d6ae67f85432d1
Christopher Lamm <chris@theantichris.com>
2019-10-04 08:39:56 -0400

    added content type

--- server.go
+++ server.go
@@ -36,9 +36,8 @@ func NewPlayerServer(store PlayerStore) *PlayerServer {
 }
 
 func (p *PlayerServer) leagueHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "application/json")
 	_ = json.NewEncoder(w).Encode(p.store.GetLeague())
-
-	w.WriteHeader(http.StatusOK)
 }
 
 func (p *PlayerServer) playerHandler(w http.ResponseWriter, r *http.Request) {
