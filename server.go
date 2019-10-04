b287742d4244cb2e8bc756b03f2709403fce2ce9
Christopher Lamm <chris@theantichris.com>
2019-10-04 09:10:54 -0400

    added comments

--- server.go
+++ server.go
@@ -9,22 +9,26 @@ import (
 const contentTypeHeader = "content-type"
 const jsonContentType = "application/json"
 
+// Player stores a name with a number of wins
 type Player struct {
 	Name string
 	Wins int
 }
 
+// PlayerStore stores score information about players
 type PlayerStore interface {
 	GetPlayerScore(name string) int
 	RecordWin(name string)
 	GetLeague() []Player
 }
 
+// PlayerServer is a HTTP interface for player information
 type PlayerServer struct {
 	store PlayerStore
 	http.Handler
 }
 
+// NewPlayerServer creates a PlayerServer with routing configured
 func NewPlayerServer(store PlayerStore) *PlayerServer {
 	p := new(PlayerServer)
 	p.store = store
