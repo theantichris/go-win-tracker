90354e823514ec0df5cd494a2ce1bbb68df6824a
Christopher Lamm <chris@theantichris.com>
2019-10-08 15:09:24 -0400

    file system player store factory

--- FileSystemPlayerStore.go
+++ FileSystemPlayerStore.go
@@ -48,6 +48,26 @@ func initialisePlayerDBFile(file *os.File) error {
 	return nil
 }
 
+func FileSystemPlayerStoreFromFile(path string) (*FileSystemPlayerStore, func(), error) {
+	db, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE, 0666)
+
+	if err != nil {
+		return nil, nil, fmt.Errorf("problem opening %s %v", path, err)
+	}
+
+	closeFunc := func() {
+		_ = db.Close()
+	}
+
+	store, err := NewFileSystemPlayerStore(db)
+
+	if err != nil {
+		return nil, nil, fmt.Errorf("problem creating file system player store %v", err)
+	}
+
+	return store, closeFunc, nil
+}
+
 func (f *FileSystemPlayerStore) GetLeague() League {
 	sort.Slice(f.league, func(i, j int) bool {
 		return f.league[i].Wins > f.league[j].Wins
--- cmd/webserver/main.go
+++ cmd/webserver/main.go
@@ -10,17 +10,13 @@ import (
 const dbFileName = "game.db.json"
 
 func main() {
-	db, err := os.OpenFile(dbFileName, os.O_RDWR|os.O_CREATE, 0666)
+	store, closeFile, err := poker.FileSystemPlayerStoreFromFile(dbFileName)
 
 	if err != nil {
-		log.Fatalf("problem opening %s %v", dbFileName, err)
+		log.Fatal(err)
 	}
 
-	store, err := poker.NewFileSystemPlayerStore(db)
-
-	if err != nil {
-		log.Fatalf("problem creating file system player store, %v", err)
-	}
+	defer closeFile()
 
 	server := poker.NewPlayerServer(store)
 
