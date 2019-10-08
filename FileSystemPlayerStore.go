24ba9d9ed6a20ac32253b0c8288b4b69e5198721
Christopher Lamm <chris@theantichris.com>
2019-10-08 08:05:16 -0400

    handling empty file

--- FileSystemPlayerStore.go
+++ FileSystemPlayerStore.go
@@ -13,6 +13,16 @@ type FileSystemPlayerStore struct {
 
 func NewFileSystemPlayerStore(file *os.File) (*FileSystemPlayerStore, error) {
 	_, _ = file.Seek(0, 0)
+	info, err := file.Stat()
+
+	if err != nil {
+		return nil, fmt.Errorf("problem getting file info from from file %s, %v", file.Name(), err)
+	}
+
+	if info.Size() == 0 {
+		_, _ = file.Write([]byte("[]"))
+		_, _ = file.Seek(0, 0)
+	}
 
 	league, err := NewLeague(file)
 
