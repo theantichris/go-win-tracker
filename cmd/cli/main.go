1c31f4b31dcdfd7f6a08b6c8455267faeaef6760
Christopher Lamm <chris@theantichris.com>
2019-10-09 15:22:52 -0400

    cleanup

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -21,6 +21,7 @@ func main() {
 	fmt.Println("Let's play poker.")
 	fmt.Println("Type {Name} wins to record a win")
 
-	game := poker.NewCLI(os.Stdin, os.Stdout, poker.NewGame(poker.BlindAlerterFunc(poker.StdOutAlerter), store))
-	game.PlayPoker()
+	game := poker.NewGame(poker.BlindAlerterFunc(poker.StdOutAlerter), store)
+	cli := poker.NewCLI(os.Stdin, os.Stdout, game)
+	cli.PlayPoker()
 }
